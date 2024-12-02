2e63d5ac71a803fb9d88d501d0c25fe7d0fc9de2
Mewtwocoder <130798336+Mewtwocoder1@users.noreply.github.com>
2024-12-02 09:24:18 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
+const registerServiceWorker = async () => {
+  if ("serviceWorker" in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register("/sw.js", {
+        scope: "/",
+      });
+      if (registration.installing) {
+        console.log("Service worker installing");
+      } else if (registration.waiting) {
+        console.log("Service worker installed");
+      } else if (registration.active) {
+        console.log("Service worker active");
+      }
+    } catch (error) {
+      console.error(`Registration failed with ${error}`);
+    }
+  }
+};
+//End of service worker setup
+
 require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@latest/min/vs' }});
 window.MonacoEnvironment = { getWorkerUrl: () => proxy };
 
@@ -19,12 +39,4 @@ require(["vs/editor/editor.main"], function () {
 		theme: 'vs-dark'
 	});
 });
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err))
-  })
-}
+registerServiceWorker();
