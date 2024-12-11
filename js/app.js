9fb4b176364d5e9df0fd26df06cb419948b7e18d
Mewtwocoder <130798336+Mewtwocoder1@users.noreply.github.com>
2024-12-11 11:21:30 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,26 +1,20 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js')
-    .then(() => console.log('Service Worker Registered!'))
-    .catch(err => console.error('Service Worker Registration Failed:', err));
-}
-//End of service worker setup
+// Service worker registration is handled in index.html
 
 require.config({ paths: { 'vs': 'https://cdn.jsdelivr.net/npm/monaco-editor@0.43.0/min/vs' } });
 
-    require(['vs/editor/editor.main'], function () {
-      // Create the Monaco editor instance
-      const editor = monaco.editor.create(document.getElementById('editor'), {
-        value: '',
-        language: 'html',
-	theme: 'vs-dark'
-      });
+require(['vs/editor/editor.main'], function () {
+  // Create the Monaco editor instance
+  const editor = monaco.editor.create(document.getElementById('editor'), {
+    value: '',
+    language: 'html',
+    theme: 'vs-dark'
+  });
 
-      // Initialize Emmet for Monaco
-      emmetMonaco.emmetHTML(monaco);
+  // Initialize Emmet for Monaco
+  emmetMonaco.emmetHTML(monaco);
 
-      // Optional: You can trigger Emmet expansion using shortcut
-      editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.Space, function() {
-        emmetMonaco.expandAbbreviation(editor);
-      });
-    });
-registerServiceWorker();
+  // Optional: You can trigger Emmet expansion using a shortcut
+  editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.Space, function() {
+    emmetMonaco.expandAbbreviation(editor);
+  });
+});
